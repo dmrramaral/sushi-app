a9fdd5236966f612bbea8c650fc8e8a4741e656a
Amaral <dmrramaral@gmail.com>
2025-10-09 20:07:55 -0300

    Feat: Adiciona configuração de porta no .env, novas dependências e atualiza o script de start no package.json, além de incluir a página de registro no App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import NavMenu from './components/NavMenu/index';
 import Home from './pages/home/index';
 import Login from './pages/login/index';
 import NotFound from './pages/notFound';
+import Register from './pages/register/index';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/cardapio" element={<h1 style={{ padding: '100px 20px', textAlign: 'center' }}>Página do Cardápio em construção</h1>} />
         <Route path="/sobre" element={<h1 style={{ padding: '100px 20px', textAlign: 'center' }}>Página Sobre em construção</h1>} />
         <Route path="/contato" element={<h1 style={{ padding: '100px 20px', textAlign: 'center' }}>Página de Contato em construção</h1>} />
