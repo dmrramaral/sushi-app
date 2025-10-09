f2dd650196380651264aef861fa363843de519ee
Amaral <dmrramaral@gmail.com>
2025-10-09 15:38:47 -0300

    Adiciona React Router e remove arquivos não utilizados
    - Adiciona dependências `react-router` e `react-router-dom` no package.json e package-lock.json.
    - Atualiza o componente App para incluir rotas e navegação.
    - Remove arquivos de teste e logo não utilizados.
    - Aplica estilos globais no App.css.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import NavMenu from './components/NavMenu/index';
+import Home from './pages/home/index';
+import Login from './pages/login/index';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="app-container">
+      <NavMenu />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cardapio" element={<h1 style={{ padding: '100px 20px', textAlign: 'center' }}>Página do Cardápio em construção</h1>} />
+          <Route path="/sobre" element={<h1 style={{ padding: '100px 20px', textAlign: 'center' }}>Página Sobre em construção</h1>} />
+          <Route path="/contato" element={<h1 style={{ padding: '100px 20px', textAlign: 'center' }}>Página de Contato em construção</h1>} />
+        </Routes>
+      </div>
   );
 }
 
